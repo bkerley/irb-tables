918bf66bc26e30c6ed9f7b02054f9b4f31a65af6
Bryce Kerley <bryce@worldmedia.net>
2009-05-04 18:01:37 -0400

    prepare to use the testing tool

--- test/irb_tables_test.rb
+++ test/irb_tables_test.rb
@@ -4,4 +4,9 @@ class IrbTablesTest < Test::Unit::TestCase
   should "output an array as multiple lines" do
     assert_equal "[\n1\n2\n3\n]", [1, 2, 3].inspect
   end
+  context 'with an array of activerecord objects' do
+    setup do
+      a = Mug.all
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ require 'shoulda'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'irb_tables'
+require 'fixtures/ar_scaffold'
 
 class Test::Unit::TestCase
 end
